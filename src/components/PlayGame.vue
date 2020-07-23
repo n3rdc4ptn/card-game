250d718b185d2dfb33fcbeb86e9bd2af93c1ab91
Reich <moritz.reich@sap.com>
2020-07-23 15:49:32 +0200

    Revert "tag01"
    This reverts commit a37317dc058d398d21a32b582b71ccac885c8645.

--- src/components/PlayGame.vue
+++ src/components/PlayGame.vue
@@ -1,6 +1,17 @@
 <template>
   <v-carousel height="100%" hide-delimiters show-arrows-on-hover>
-    
+    <v-carousel-item v-for="(card, i) in cards" :key="i">
+      <v-sheet height="100%" :color="card.color" :style="card.textcolor ? `color:${card.textcolor};` : null" tile>
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="textContainer text" >{{ card.text }}</div>
+        </v-row>
+        <v-row class="mt-n15" align="end" justify="center">
+          <div class="cardNumber">{{ i+1 }} / {{ cards.length }}</div>
+          <v-btn class="ml-5" v-text="i == cards.length-1 ? 'Zurück zum Start' : 'Abbrechen'" @click="$emit('back_to_start')"></v-btn>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -1,6 +1,27 @@
 <template>
   <v-container>
-    
+    <v-combobox
+      v-model="players"
+      label="Players"
+      hint="Mindestens 2 Spieler angeben"
+      persistent-hint
+      multiple
+      chips
+    >
+      <template v-slot:selection="data">
+        <v-chip
+          :key="JSON.stringify(data.item)"
+          v-bind="data.attrs"
+          :input-value="data.selected"
+          :disabled="data.disabled"
+          close
+          @click:close="data.parent.selectItem(data.item)"
+        >
+          <v-avatar class="accent white--text" left v-text="data.item.slice(0, 1).toUpperCase()"></v-avatar>
+          {{ data.item }}
+        </v-chip>
+      </template>
+    </v-combobox>
 
     <v-card-actions>
       <v-spacer></v-spacer>
